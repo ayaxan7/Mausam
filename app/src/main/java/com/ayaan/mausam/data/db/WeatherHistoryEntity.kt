49c1b8389dc8f0625f68c77a248ed502b43f506e
Syed Mohammad Ayaan <syedayaan9376@gmail.com>
2026-04-09 23:29:47 +0530

    Define WEATHER_HISTORY_DB constant and use it in WeatherHistoryEntity

--- app/src/main/java/com/ayaan/mausam/data/db/WeatherHistoryEntity.kt
+++ app/src/main/java/com/ayaan/mausam/data/db/WeatherHistoryEntity.kt
@@ -2,8 +2,9 @@ package com.ayaan.mausam.data.db
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.ayaan.mausam.util.Constants.WEATHER_HISTORY_DB
 
-@Entity(tableName = "weather_history")
+@Entity(tableName = WEATHER_HISTORY_DB)
 data class WeatherHistoryEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
--- app/src/main/java/com/ayaan/mausam/util/Constants.kt
+++ app/src/main/java/com/ayaan/mausam/util/Constants.kt
@@ -5,5 +5,6 @@ object Constants {
     const val OSM_BASE_URL = "https://nominatim.openstreetmap.org/"
     const val OSM_USER_AGENT = "MausamApp/1.0 (Android)"
     const val UNITS = "metric"
+    const val WEATHER_HISTORY_DB="weather_history"
     const val ICON_URL = "https://openweathermap.org/img/wn/%s@2x.png"
 }
